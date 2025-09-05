3a5519219e7b08bc54b900c263bb288214501d05
houstoncardinal <hunainqureshicardinal@gmail.com>
2025-09-05 17:29:51 -0500

    Increase form z-index to 9999 (one less than header)

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -340,7 +340,7 @@ const HeroSlider = () => {
       </section>
 
       {/* Evaluate Your Project for Free Form - Ultra High Z-Index */}
-      <section className="relative mt-8 sm:mt-12 lg:mt-16 mb-20 sm:mb-24 lg:mb-32 z-[50]">
+      <section className="relative mt-8 sm:mt-12 lg:mt-16 mb-20 sm:mb-24 lg:mb-32 z-[9999]">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           {/* Premium Form Container with Enhanced Glow */}
           <div className="relative group">
